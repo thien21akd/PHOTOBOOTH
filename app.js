81bc65675bf7204025b39cadfc535555df9bb209
Đào Công Thiện <158730037+thien21akd@users.noreply.github.com>
2025-05-30 17:54:30 +0700

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-const serverConfigged = require("./server.config.js");
-const routesConfigged = require("./Source/Routes/routes.js");
-const PORT = 3300;
-const app = serverConfigged();
-
-routesConfigged(app);
-app.listen(PORT, () => {
-  console.log("Server đang chạy ở cổng: ", PORT);
-});
